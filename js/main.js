e91ea007c3e7fc800bd0f39de62eaa60e8c156ab
WilliamD47 <69767121+WilliamD47@users.noreply.github.com>
2021-11-30 20:13:39 +0000

    Added Variable Lengths

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function addToHTML(person) {
   var name = person["name"];
   var next = person["next"];
-
+  var day = person["day"];
   var table = document.getElementById("tbody");
 
   // Create an empty <tr> element and add it to the 1st position of the table:
@@ -12,7 +12,21 @@ function addToHTML(person) {
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   cell1.innerHTML = name;
-  cell2.innerHTML = '<table border="0" summary=""> <tr align= "right"> <td CT</td> <td>PET</td> <td>MDT</td> </tr> <tr id="tr"> <th>-</th> <td class="grn" style="width: 200px"><div class="tooltip">..<span class="tooltiptext">Day 3</span></div></td> <td class="red" style="width: 300px"><div class="tooltip">..<span class="tooltiptext">Day 10</span></div></td> <td class="blu" style="width: 100px"><div class="tooltip">..<span class="tooltiptext">Day 27</span></div></td> </tr> </table>';
+  var dayhtml = ""
+  let part1HTML = '<td class="grn" style="width: '+(day*10)+'px"><div class="tooltip">PET<span class="tooltiptext">Day '+String(day)+'</span></div></td>'
+  let part2HTML = part1HTML + '<td class="red" style="width: '+(day*10)+'px"><div class="tooltip">MDT<span class="tooltiptext">Day '+String(day)+'</span></div></td>'
+  let part3HTML = part2HTML + '<td class="blu" style="width: '+(day*10)+'px"><div class="tooltip">Consultation<span class="tooltiptext">Day '+String(day)+'</span></div></td>'
+
+  if (day > 20) {
+    dayhtml += part3HTML
+  } else if (day > 10) {
+    dayhtml += part2HTML
+  } else if (day > 1) {
+    dayhtml += part1HTML
+  }
+
+    
+  cell2.innerHTML = '<table border="0" summary=""> <tr align= "right"> <td CT</td> <td>PET</td> <td>MDT</td> </tr> <tr id="tr"> <th>-</th>'+dayhtml+'</tr></table>';
   cell3.innerHTML = next;
 }
 
@@ -20,28 +34,32 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
+    navigator.serviceWorker 
               .register('./sw.js');
   }
   var people = [
     {
-        "name" : "Joe",
+      "name" : "Joe",
         "stage" : 3,
+        "day" : 23,
         "next" : "MDT"
     },
     {
         "name" : "Eric",
         "stage" : 4,
+        "day" : 35,
         "next" : "Consultation"
     },
     {
         "name" : "Sarah",
         "stage" : 1,
+        "day" : 0,
         "next" : "Consultation"
     },
     {
         "name" : "John",
         "stage" : 2,
+        "day" : 16,
         "next" : "test"
     }
   ]
@@ -49,10 +67,3 @@ window.onload = () => {
   people.forEach(element => addToHTML(element));
 }
 
-function wait(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
